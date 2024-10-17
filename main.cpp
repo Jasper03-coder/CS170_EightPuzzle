0542e10f079a479a89284cf1b8216df7c6867833
Kimberly Monteagudo <kimberlymonteagudo@ucr-secure-25-10-13-150-202.wnet.ucr.edu>
2024-10-17 13:58:14 -0700

    Input/Output

---
+++ main.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include<sstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    cout << "Welcome to 862288730, XXX, and XXX 8 puzzle solver." << endl;
+
+    cout << "Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle." << endl;
+    int puzzleType;
+    cin >> puzzleType;
+
+    while (puzzleType != 1 && puzzleType != 2) {
+        cout << "\nPlease type a valid digit" << endl;
+        cout << "Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle." << endl;
+        cin >> puzzleType;
+    }
+    
+    vector< vector<int> > initial; 
+
+    if (puzzleType == 1) { // Default puzzle ---------------
+        
+        vector<int> row;
+        row.push_back(1);
+        row.push_back(2);
+        row.push_back(3);
+        initial.push_back(row);
+
+        row.clear();
+        row.push_back(4);
+        row.push_back(8);
+        row.push_back(5);
+        initial.push_back(row);
+
+        row.clear();
+        row.push_back(7);
+        row.push_back(0);
+        row.push_back(6);
+        initial.push_back(row);
+
+    }
+    else if (puzzleType == 2) { // Custom puzzle ----------------
+
+        cout << "Enter your puzzle, use a zero to represent the blank" << endl;
+        cout << "Enter the first row, use space or tabs between numbers" << endl;
+        
+        string row;
+        stringstream ss;
+
+        cin.ignore();
+        getline(cin, row);
+        ss << row;
+        int value;
+        
+        vector<int> row1;
+        for (int i = 0; i < 3; i++) {
+            ss >> value;
+            row1.push_back(value);
+        }
+        initial.push_back(row1);
+
+        ss.clear();
+        vector<int> row2;
+        for (int i = 0; i < 3; i++) {
+            ss >> value;
+            row2.push_back(value);
+        }
+        initial.push_back(row2);
+
+        ss.clear();
+        vector<int> row3;
+        for (int i = 0; i < 3; i++) {
+            ss >> value;
+            row3.push_back(value);
+        }
+        initial.push_back(row3);
+
+    }
+
+    /*
+    
+        Add code if necessary 
+    
+    */
+
+    cout << "\nEnter your choice of algorithm" << endl;
+    cout << "1 for Uniform Cost Search" << endl;
+    cout << "2 for A* with the Misplaced Tile Heuristic" << endl;
+    cout << "3 for A* with the Euclidean distance Heuristic" << endl;
+
+    int algorithm;
+    cin >> algorithm;
+
+    while (algorithm != 1 && algorithm != 2 && algorithm != 3) {
+        cout << "\nPlease enter a valid digit" << endl;
+        cout << "\nEnter your choice of algorithm" << endl;
+        cout << "1 for Uniform Cost Search" << endl;
+        cout << "2 for A* with the Misplaced Tile Heuristic" << endl;
+        cout << "3 for A* with the Euclidean distance Heuristic" << endl;
+
+        cin >> algorithm;
+    }
+
+    /*
+    
+        Add code if necessary
+    
+    */
+
+    int maxQueue;
+    int nodesExpanded;
+    int nodeDepth;
+
+    // run the algorithms here
+    
+    if (algorithm == 1) { // Uniform Cost Search
+
+    }
+    else if (algorithm == 2) { // Misplaced Tile 
+
+    }
+    else if (algorithm == 3) { // Euclidean Distance
+         
+    }
+
+    
+    // cout << "To solve this problem the search algorithm expanded a total of " << nodesExpanded << " nodes." << endl;
+    // cout << "The maximum number of nodes in the queue at any one time: " << maxQueue << endl;
+    // cout << "The depth of the goal was " << nodeDepth << endl;
+   
+
+    return 0;
+}
